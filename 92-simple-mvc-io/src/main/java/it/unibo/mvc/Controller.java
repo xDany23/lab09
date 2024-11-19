94d24ff44b8f14000fc252f4dc217c340909a51c
Daniele Tramonti <daniele.tramonti2@studio.unibo.it>
2024-11-19 16:41:35 +0100

    Ho iniziato il secondo esercizio

--- 92-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
+++ 92-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
@@ -1,9 +1,43 @@
 package it.unibo.mvc;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import javax.swing.JOptionPane;
 
 /**
  * Application controller. Performs the I/O.
  */
 public class Controller {
+    private final String USER = System.getProperty("user.home");
+    private final String SEPARATOR = System.getProperty("user.separator");
+    private File currentFile = new File(USER + SEPARATOR + "output.txt");
+
+    public void setDefault(File file) {
+        this.currentFile = file;
+    }
+
+    public File getCurrentFile() {
+        return this.currentFile;
+    }
+
+    public String getCurrentFilePath() {
+        return this.currentFile.getAbsolutePath();
+    }
+
+    public void WriteOnFile(String string) throws IOException {
+       try (PrintStream ps = new PrintStream(this.currentFile, StandardCharsets.UTF_8)) {
+            ps.print(string);
+        } catch (IOException e1) {
+            System.out.println("ERROR");
+            e1.printStackTrace(); // NOPMD: allowed as this is just an exercise
+        }
+    }
+
+
 
 }
